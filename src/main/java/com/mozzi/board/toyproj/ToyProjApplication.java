95a000401e809a899ccd0f0ba8b6f57913fdc838
mozzi327 <mozzi327@gmail.com>
2022-08-24 03:10:54 +0900

    aws프로젝트 시작

---
+++ src/main/java/com/mozzi/board/toyproj/ToyProjApplication.java
@@ -0,0 +1,13 @@
+package com.mozzi.board.toyproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mozzi/board/toyproj/ToyProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozzi.board.toyproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
